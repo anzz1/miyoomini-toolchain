28c7429fc693d6e0d41d6f9a5db509cf2d14a64d
anzz1 <anzz1@live.com>
2023-05-30 23:55:37 +0300

    fix dir

--- support/setup-env.sh
+++ support/setup-env.sh
@@ -3,5 +3,5 @@ export CROSS_COMPILE=/opt/miyoomini-toolchain/usr/bin/arm-linux-gnueabihf-
 export PREFIX=/opt/miyoomini-toolchain/usr/arm-linux-gnueabihf/sysroot/usr
 export SYSROOT=/opt/miyoomini-toolchain/arm-linux-gnueabihf/sysroot
 export UNION_PLATFORM=miyoomini
-source ~/xldd.sh
-source ~/xcross.sh
+source /root/xldd.sh
+source /root/xcross.sh
